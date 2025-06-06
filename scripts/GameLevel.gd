abef56e341e3e7fbcf5bed9ddedaa632f80ab0b8
LeZellus <matheo.zeller@gmail.com>
2025-06-06 23:53:01 +0200

    main menu added

--- scripts/GameLevel.gd
+++ scripts/GameLevel.gd
@@ -5,7 +5,30 @@ extends Node2D
 @export var starting_room: String = ""
 @export var player_spawn_position: Vector2 = Vector2(150, 220)
 
+# À ajouter dans votre GameLevel.gd après les @export
+@onready var menu_layer: CanvasLayer = $MenuLayer
+@onready var main_menu: Control = $MenuLayer/MainMenu
+
+# Modifier votre fonction _ready() existante :
 func _ready():
+	# NOUVEAU : Connecter le signal du menu
+	if main_menu:
+		main_menu.play_requested.connect(_start_game)
+	
+	# Commencer par afficher le menu
+	_show_menu()
+
+# NOUVELLES fonctions à ajouter :
+func _show_menu():
+	if menu_layer:
+		menu_layer.visible = true
+
+func _start_game():
+	print("Démarrage du jeu...")
+	if menu_layer:
+		menu_layer.visible = false
+	
+	# VOTRE CODE EXISTANT (déplacé ici depuis _ready)
 	if not starting_world:
 		push_error("Aucun monde de départ défini!")
 		return
@@ -15,7 +38,6 @@ func _ready():
 	SceneManager.initialize_with_player(player_scene)
 	await SceneManager.load_world(starting_world, starting_room)
 	
-	# Position initiale du joueur
 	if SceneManager.player:
 		SceneManager.player.global_position = player_spawn_position
 		SceneManager.player.velocity = Vector2.ZERO
---
+++ scripts/ui/MainMenu.gd
@@ -0,0 +1,25 @@
+extends Control
+
+signal play_requested
+
+@onready var play_button: Button = $CenterContainer/VBoxContainer/PlayButton
+@onready var settings_button: Button = $CenterContainer/VBoxContainer/SettingsButton
+@onready var quit_button: Button = $CenterContainer/VBoxContainer/QuitButton
+
+func _ready():
+	# Connecter les boutons
+	play_button.pressed.connect(_on_play_pressed)
+	settings_button.pressed.connect(_on_settings_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+
+func _on_play_pressed():
+	print("Bouton Jouer pressé")
+	play_requested.emit()
+
+func _on_settings_pressed():
+	print("Bouton Paramètres pressé - TODO: Ouvrir paramètres")
+	# TODO: Ouvrir menu paramètres
+
+func _on_quit_pressed():
+	print("Bouton Quitter pressé")
+	get_tree().quit()
